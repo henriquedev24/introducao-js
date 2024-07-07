191f5f944339ecb7a1e1921b0a8a2fb35a861efe
Gabriel Henrique <henriquedeveloper24@gmail.com>
2024-07-07 19:40:17 -0300

    testando

--- exercicios-condicionais/script2.js
+++ exercicios-condicionais/script2.js
@@ -2,23 +2,29 @@
 favorável. Essa situação é determinada através do IMC (Índice de Massa Corpórea), que é
 definida como sendo a relação entre o peso (PESO) e o quadrado da Altura (ALTURA) do
 indivíduo */
+function calcularIMC(altura, peso) {
+    return peso / (altura ** 2); 
+}
 
-function calcularIMC(imc) {
+function classifIMC(altura, peso) {
+    var imc = calcularIMC(altura, peso);
     if (imc < 20) {
-        console.log(imc, " Está abaixo do peso")
+        return "IMC: " + imc.toFixed(1) + " - Está abaixo do peso";
     } else if (imc >= 20 && imc <= 25) {
-        console.log(imc," Peso Normal")
-    } else if (imc >= 25 && imc <= 30) {
-        console.log(imc, " Sobre Peso")
-    } else if (imc >= 30 && imc <= 40) {
-        console.log(imc, " Obeso")
-    } else if (imc >= 40) {
-        console.log(imc, " Obeso Mórbido")
+        return "IMC: " + imc.toFixed(1) + " - Peso Normal";
+    } else if (imc > 25 && imc <= 30) {
+        return "IMC: " + imc.toFixed(1) + " - Sobrepeso";
+    } else if (imc > 30 && imc <= 40) {
+        return "IMC: " + imc.toFixed(1) + " - Obeso";
+    } else if (imc > 40) {
+        return "IMC: " + imc.toFixed(1) + " - Obeso Mórbido";
     }
 }
 
-let altura = parseFloat(prompt("Altura: "))
-let peso = parseFloat(prompt("Peso: "))
-let imc = peso / (altura^2)
-let resultado = calcularIMC(imc)
-console.log(resultado)
+var altura = parseFloat(prompt("Altura: "));
+var peso = parseFloat(prompt("Peso: "));
+
+ 
+var resultado = classifIMC(altura, peso); 
+
+console.log(resultado);
--- exercicios-condicionais/script7.js
+++ exercicios-condicionais/script7.js
@@ -1,7 +1,6 @@
 /* 7. Construa um algoritmo que determine (imprima) se um dado número N
 inteiro (recebido através do teclado) é PAR ou ÍMPAR. */
 
-
 function ehPar(numero) {
     if (numero % 2 == 0){
         console.log("O número é par")
@@ -9,3 +8,5 @@ function ehPar(numero) {
         console.log("O número é ímpar")
     }
 }
+
+ehPar(5)
