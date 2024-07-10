bcd1941e836f7c78cc62fa3a6deee52c37495902
Gabriel Henrique <henriquedeveloper24@gamil.com>
2024-07-10 11:02:02 -0300

    add

---
+++ metodos-de-objetos/index.js
@@ -0,0 +1,12 @@
+
+// JSON.
+
+
+var objA = {
+    nome: 'gabriel',
+    idade: 20,
+    hobbies: ['Jogar', 'Correr']
+}
+// console.log(JSON.stringify(objA))
+// Object.keys(objA).forEach(item => console.log(objA[item]))
+console.log(Object.values(objA))
