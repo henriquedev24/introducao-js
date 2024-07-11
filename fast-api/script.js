ae626222b21e16e9c401293003744dda92f2ce40
Gabriel Henrique <henriquedeveloper24@gamil.com>
2024-07-11 11:24:33 -0300

    aula sobre fetch api

---
+++ fast-api/script.js
@@ -0,0 +1,21 @@
+// Fetch API
+
+const btn = document.getElementById('btn');
+const output = document.getElementById('output');
+
+btn.addEventListener('click', () => {
+    fetch('https://63e7c552ac3920ad5be36b95.mockapi.io/produtos')
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(item => {
+                output.innerHTML += `
+                <img width="300" src='${item.img}' alt="${item.titulo}" />
+                <p>${item.titulo}</p>
+                <div>
+                <s>${item.valor}</s>
+                <p>${item.desconto}</p>
+                </div>
+                `;
+            })
+        })
+});
